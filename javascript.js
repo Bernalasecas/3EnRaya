1acba8a9c6d73e582f78ddd6f017ddb663b67c53
Bernal <b3rn4lrc@gmail.com>
2024-04-22 19:28:03 +0200

    Logica acabada

--- javascript.js
+++ javascript.js
@@ -1,4 +1,29 @@
+var casillasJugadas = {};
+var contador = 0;
+
 function jugar(id) {
-    var casilla = document.getElementById(id);
-    casilla.style.backgroundColor = "black";
-};
+    // Verificamos si la casilla ya ha sido jugada
+    if (!casillasJugadas[id]) {
+
+        var casilla = document.getElementById(id);
+        var circulo = casilla.querySelector('.circulo');
+        var cuadrado = casilla.querySelector('.cuadrado');
+
+        if (contador === 0) {
+            circulo.style.display = 'block';
+            cuadrado.style.display = 'none';
+            contador = 1;
+        } else {
+            circulo.style.display = 'none';
+            cuadrado.style.display = 'block';
+            contador = 0;
+        }
+
+        casillasJugadas[id] = true;
+        return contador;
+    }
+}
+
+function recargar(){
+    location.reload();
+}
